225bac3f63a7035661f01fc975b11547f8fbb130
manyids2 <arun.mukundan@gmail.com>
2024-03-07 11:36:12 +0100

    Better names

--- grammars/xrandr/baseline/grammar.js
+++ grammars/xrandr/baseline/grammar.js
@@ -1,19 +1,13 @@
+const num_with_units = (res) => seq(res, field("units", token.immediate(/[a-zA-Z]+/)));
+
 module.exports = grammar({
   name: "xrandr",
-  conflicts: ($) => [[$._resolution_line]],
   rules: {
     document: ($) => repeat(choice($.screen, $.disconnected, $.connected)),
-    number: ($) => /[\d\.]+/,
-    xres: ($) => /[\d\.]+/,
-    yres: ($) => /[\d\.]+/,
-    resolution: ($) => prec.left(1, seq($.xres, 'x', $.yres)),
-    geometry: ($) => seq(
-      $.number, token.immediate('x'),
-      $.number, token.immediate('+'),
-      $.number, token.immediate('+'),
-      $.number
-    ),
-    display: ($) => /[a-zA-Z0-9-]+-[0-9]+/,
+    number: (_) => /[\d\.]+/,
+    xres: ($) => alias($.number, "xres"),
+    yres: ($) => alias($.number, "yres"),
+    resolution: ($) => seq($.xres, 'x', $.yres),
     // Screen
     screen: ($) => seq(
       seq('Screen', field("screen_number", $.number)), ':',
@@ -21,24 +15,33 @@ module.exports = grammar({
       'current', field('current', $.resolution), ',',
       'maximum', field('maximum', $.resolution),
     ),
+    display: (_) => /[a-zA-Z0-9-]+-[0-9]+/,
     // Disconnected
-    props: ($) => seq('(', repeat1(choice('normal', 'left', 'right', 'x', 'y', 'axis', 'inverted')), ')'),
-    primary: ($) => 'primary',
+    props: (_) => seq('(', repeat1(choice('normal', 'left', 'right', 'x', 'y', 'axis', 'inverted')), ')'),
+    primary: (_) => 'primary',
     disconnected: ($) => seq(
-      field('name', $.display), 'disconnected', repeat(field('primary', $.primary)),
+      field('display', $.display), 'disconnected', repeat(field('primary', $.primary)),
       $.props,
     ),
     // Connected
-    _number_with_units: ($) => seq($.number, field("units", token.immediate(/[a-zA-Z]+/))),
-    resolution_with_units: ($) => seq($._number_with_units, 'x', $._number_with_units),
-    mark: ($) => choice(token.immediate('+'), token.immediate('*')),
-    _resolution_line: ($) => seq($.resolution, repeat1(choice($.number, $.mark))),
+    xoff: ($) => alias($.number, "xoff"),
+    yoff: ($) => alias($.number, "yoff"),
+    geometry: ($) => seq(
+      $.xres, token.immediate('x'),
+      $.yres, token.immediate('+'),
+      $.xoff, token.immediate('+'),
+      $.yoff
+    ),
+    resolution_with_units: ($) => seq(num_with_units($.xres), 'x', num_with_units($.yres)),
+    mark: (_) => choice(token.immediate('+'), token.immediate('*')),
+    // -- may not always be terminated by \n?
+    _resolution_line: ($) => seq($.resolution, repeat1(choice($.number, $.mark)), '\n'),
     resolutions: ($) => repeat1($._resolution_line),
     connected: ($) => seq(
-      field('name', $.display), 'connected', repeat(field('primary', $.primary)), $.geometry,
+      field('display', $.display), 'connected', repeat(field('primary', $.primary)), $.geometry,
       $.props,
       $.resolution_with_units,
-      optional($.resolutions),
+      $.resolutions,
     ),
   },
 });
